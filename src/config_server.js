6a90faaed0ebc2d786b7aae18ade0ec45274f8c8
dinhvandong <dongvan.ict3@gmail.com>
2021-04-27 14:53:39 +0700

    add

--- src/config_server.js
+++ src/config_server.js
@@ -1,4 +1,4 @@
 import React from 'react';
-const ConfigServer = {host:"http://localhost:8080", host_asset:"http://localhost:8080"};
+const ConfigServer = {host:"http://128.199.145.159:8080", host_asset:"http://128.199.145.159:8080"};
 //http://dev.codekitten.asia
 export default ConfigServer;
--- src/lib/get-costume-url.js
+++ src/lib/get-costume-url.js
@@ -8,32 +8,35 @@ const getCostumeUrl = (function () {
     let cachedAssetId;
     let cachedUrl;
     return function (asset) {
-        // if (cachedAssetId === asset.assetId)
-        // {
-        //     return cachedUrl;
-        // }
-        // cachedAssetId = asset.assetId;
+        if (cachedAssetId === asset.assetId)
+        {
+            return cachedUrl;
+        }
+        cachedAssetId = asset.assetId;
         // If the SVG refers to fonts, they must be inlined in order to display correctly in the img tag.
         // Avoid parsing the SVG when possible, since it's expensive.
-        // if (asset.assetType === storage.AssetType.ImageVector) {
-        //     const svgString = asset.decodeText();
-        //     if (svgString.match(HAS_FONT_REGEXP)) 
-        //     {
-        //         const svgText = inlineSvgFonts(svgString);
-        //         cachedUrl = `data:image/svg+xml;utf8,${encodeURIComponent(svgText)}`;
-        //     } else 
-        //     {
-        //         cachedUrl = asset.encodeDataURI();
-        //     }
-        // } else 
-        // {
-        //     cachedUrl = asset.encodeDataURI();
-        // }
+        if (asset.assetType === storage.AssetType.ImageVector) {
+            const svgString = asset.decodeText();
+            if (svgString.match(HAS_FONT_REGEXP)) 
+            {
+                const svgText = inlineSvgFonts(svgString);
+                cachedUrl = `data:image/svg+xml;utf8,${encodeURIComponent(svgText)}`;
+            } else 
+            {
+                cachedUrl = asset.encodeDataURI();
+            }
+        } else 
+        {
+            cachedUrl = asset.encodeDataURI();
+        }
+
+
         var link_download =  ConfigServer.host + "/api/asset/find/" + asset.assetId;
 
-        console.log("link_download",link_download);
+        console.log("link_download",asset);
+        //8e84b654a7b457f5c97d16ac255d6dee
         
-        return link_download;
+        return cachedUrl;
     };
 }());
 export {
